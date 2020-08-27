1c05631523ab7f2ee1360cb2185fb4611c2249c1
Matheus Santos <matheussm301@gmail.com>
2020-08-26 22:38:51 -0300

    chore: Add modal in create naver page

--- src/pages/CreateNaver/index.tsx
+++ src/pages/CreateNaver/index.tsx
@@ -1,11 +1,13 @@
-import React, { useCallback, useRef } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React, { useCallback, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import { FaAngleLeft } from 'react-icons/fa';
+import { Container, Title, InputGroup } from './styles';
+
 import Input from '../../components/Input';
 import Header from '../../components/Header';
-import { Container, Title, InputGroup } from './styles';
+import ModalMessage from '../../components/ModalMessage';
 
 import api from '../../services/api';
 
@@ -20,8 +22,12 @@ interface CreateNaverFormData {
 
 const CreateNaver: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
-  const history = useHistory();
+  const [modalOpen, setModalOpen] = useState(false);
   const token = localStorage.getItem('@Navedex:token');
+
+  const toggleModal = useCallback(() => {
+    setModalOpen(!modalOpen);
+  }, [modalOpen]);
 
   const handleSubmit = useCallback(
     async (data: CreateNaverFormData) => {
@@ -30,62 +36,68 @@ const CreateNaver: React.FC = () => {
           Authorization: `Bearer ${token}`,
         },
       });
-      history.push('/navers');
+      formRef.current?.reset();
+      toggleModal();
     },
-    [history, token],
+    [token, toggleModal],
   );
+
   return (
-    <>
-      <Container>
-        <Header />
-        <Title>
-          <Link to="/navers">
-            <FaAngleLeft />
-          </Link>
-          <h2>Adicionar Naver</h2>
-        </Title>
+    <Container>
+      <Header />
+      <ModalMessage
+        title="Naver criado"
+        message="Naver Criado com sucesso!"
+        swicth={modalOpen}
+        setOpenModal={toggleModal}
+      />
+      <Title>
+        <Link to="/navers">
+          <FaAngleLeft />
+        </Link>
+        <h2>Adicionar Naver</h2>
+      </Title>
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <InputGroup>
-            <span>Nome</span>
-            <Input name="name" placeholder="Nome" type="text" />
-          </InputGroup>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <InputGroup>
+          <span>Nome</span>
+          <Input name="name" placeholder="Nome" type="text" />
+        </InputGroup>
 
-          <InputGroup>
-            <span>Cargo</span>
-            <Input name="job_role" placeholder="Cargo" type="text" />
-          </InputGroup>
-          <InputGroup>
-            <span>Idade</span>
-            <Input name="birthdate" placeholder="Idade" type="text" />
-          </InputGroup>
+        <InputGroup>
+          <span>Cargo</span>
+          <Input name="job_role" placeholder="Cargo" type="text" />
+        </InputGroup>
+        <InputGroup>
+          <span>Idade</span>
+          <Input name="birthdate" placeholder="Idade" type="text" />
+        </InputGroup>
 
-          <InputGroup>
-            <span>Tempo de empresa</span>
-            <Input
-              name="admission_date"
-              placeholder="Tempo de empresa"
-              type="text"
-            />
-          </InputGroup>
+        <InputGroup>
+          <span>Tempo de empresa</span>
+          <Input
+            name="admission_date"
+            placeholder="Tempo de empresa"
+            type="text"
+          />
+        </InputGroup>
 
-          <InputGroup>
-            <span>Projetos que participou</span>
-            <Input
-              name="project"
-              placeholder="Projetos que participou"
-              type="text"
-            />
-          </InputGroup>
+        <InputGroup>
+          <span>Projetos que participou</span>
+          <Input
+            name="project"
+            placeholder="Projetos que participou"
+            type="text"
+          />
+        </InputGroup>
 
-          <InputGroup>
-            <span>URL da foto do Naver</span>
-            <Input name="url" placeholder="URL da foto do Naver" type="text" />
-          </InputGroup>
-          <button type="submit">Salvar</button>
-        </Form>
-      </Container>
-    </>
+        <InputGroup>
+          <span>URL da foto do Naver</span>
+          <Input name="url" placeholder="URL da foto do Naver" type="text" />
+        </InputGroup>
+        <button type="submit">Salvar</button>
+      </Form>
+    </Container>
   );
 };
 
